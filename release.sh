c128408c84890882f6c2ff6a0d6911b1811b6999
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2018-08-21 21:23:19 +0200

    Update the release script

--- release.sh
+++ release.sh
@@ -3,9 +3,9 @@ set -euxo pipefail
 
 rm -rf release || exit 0;
 
-elm-package bump
+elm bump
 
-version=$(grep -m1 version elm-package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+version=$(grep -m1 version elm.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
 git commit -a -m "Bump to $version"
 git push
@@ -21,5 +21,5 @@ git clone --reference . git@github.com:elm-explorations/webgl.git release
   git commit -m "Cleanup and release $version"
   git tag -a $version -m "Release $version"
   git push origin $version
-  elm-package publish
+  elm publish
 )
